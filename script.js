bbea50dfdd05a7cc84dc2fff23e2a6fc9220d11a
baldasaree <145886713+baldasaree@users.noreply.github.com>
2023-09-24 11:01:42 +0700

    Change greeting msg

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ firstBtn.addEventListener('click', () => {
     clickCount++;
     if (clickCount === 3) {
         const para = document.createElement('p');
-        para.textContent = "Welcome to Management e[X]posed!";
+        para.textContent = "Welcome to Liaison Officer of the 18th Management e[X]posed!";
         body.appendChild(para);
     }
 })
