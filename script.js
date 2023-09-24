105bd7443c1725fe9eb55e58b16a53b39eb0bc78
baldasaree <145886713+baldasaree@users.noreply.github.com>
2023-09-24 12:11:32 +0700

    Add condition to btn

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ let clickCount = 0;
 firstBtn.addEventListener('click', () => {
 
     clickCount++;
+
+    if (clickCount === 5) {
+        firstBtn.textContent = "Almost There";
+    }
+
+    if (clickCount === 8) {
+        firstBtn.textContent = "Really Close";
+    }
+    
     if (clickCount === 10) {
         const para = document.createElement('p');
         para.textContent = "Welcome to Liaison Officer of the 18th Management e[X]posed!";
